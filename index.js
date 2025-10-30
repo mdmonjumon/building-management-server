9cb309b66e6854626a505dd449a67ad4e8c2fb4f
mdmonjumon <mdmonjumon786@gmail.com>
2025-10-30 12:21:18 +0600

    install stripe and create an api to create payment intent

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 const nodemailer = require("nodemailer");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5200;
 const app = express();
 
@@ -41,7 +43,15 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+// dollar to cent
+const handleDollarToCent = (value) => {
+  if (value < 1) {
+    return value;
+  }
+  return value * 100;
+};
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.BUILDING_DB_USER}:${process.env.BUILDING_DB_PASS}@cluster0.xt5rphe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -58,6 +68,7 @@ const db = client.db("apartmentsDB");
 const apartmentsCollection = db.collection("apartments");
 const agreementsCollection = db.collection("agreements");
 const usersCollection = db.collection("users");
+const couponsCollection = db.collection("coupon");
 
 async function run() {
   try {
@@ -119,11 +130,54 @@ async function run() {
     // get agreement data for specific user
     app.get("/agreement/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
-      const result = await agreementsCollection.findOne({userEmail:email})
+      const result = await agreementsCollection.findOne({ userEmail: email });
       res.send(result);
     });
 
+    // get coupon data
+    app.get("/coupons", async (req, res) => {
+      const result = await couponsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get single coupon data
+    app.get("/coupon/:id", verifyToken, async (req, res) => {
+      const couponId = req.params.id;
+      const result = await couponsCollection.findOne({ coupon_id: couponId });
+      res.send(result);
+    });
+
+    // save payments info in db
     
+
+    // create payment intent
+    app.post("/payment-intent", verifyToken, async (req, res) => {
+      const paymentInfo = req.body;
+      const couponQuery = { coupon_id: paymentInfo?.couponId };
+      const apartmentQuery = { apartmentId: paymentInfo?.apartmentId };
+      const apartment = await agreementsCollection.findOne(apartmentQuery);
+      const coupon = await couponsCollection.findOne(couponQuery);
+      let amount = apartment?.rent;
+      if (coupon) {
+        if (coupon?.discount_type === "percentage") {
+          amount = amount - (apartment?.rent * coupon?.value) / 100;
+        }
+        if (coupon?.discount_type === "fixed_amount") {
+          amount = amount - coupon?.value;
+        }
+      }
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: handleDollarToCent(amount),
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+
+      res.send({ clientSecret: paymentIntent?.client_secret });
+    });
+
     app.get("/", (req, res) => {
       res.send("building app running");
     });
